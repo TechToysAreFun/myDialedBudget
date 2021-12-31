4d2b2e3bf7810a736686305c1e2328e2cb2a0a7e
Matt Hart <techtoysarefun@gmail.com>
2021-12-31 08:50:30 -0800

    Update imports for 'settings'

--- application/settings/routes.py
+++ application/settings/routes.py
@@ -2,15 +2,11 @@ import os
 import secrets
 from PIL import Image
 from flask import flash, redirect, render_template, request, session, url_for, Blueprint
-from flask_mail import Message
-from application import app, nav_avatar, mail
+from application import app, nav_avatar
 from werkzeug.security import check_password_hash, generate_password_hash
-from werkzeug.utils import secure_filename
-import datetime
-import re
 from application.helpers import login_required
 from application import db
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from settings.utils import context_processor
 
 settings = Blueprint('settings', __name__)
 
@@ -329,11 +325,9 @@ def settings_usage(usage):
         global nav_avatar
         nav_avatar = session['nav_avatar']
         
+        context_processor()
+
         # Redirect to settings
         flash('Avatar successfully updated', 'success')
         return redirect(url_for('settings'))
-
-
-
-
 
