b92140ef2b1aa5c58a362f5806339295abb5899d
Matt Hart <techtoysarefun@gmail.com>
2021-12-30 19:29:26 -0800

    Copy all imports from original routes.py into all new routes.py files and delete original

--- application/history/routes.py
+++ application/history/routes.py
@@ -1,4 +1,16 @@
-from flask import Blueprint
+import os
+import secrets
+from PIL import Image
+from flask import flash, redirect, render_template, request, session, url_for, Blueprint
+from flask_mail import Message
+from application import app, nav_avatar, mail
+from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.utils import secure_filename
+import datetime
+import re
+from application.helpers import login_required
+from application import db
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 history = Blueprint('history', __name__)
 
