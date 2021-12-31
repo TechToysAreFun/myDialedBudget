e15eb6b2d213a6a23d6ba682aadf9b3f7d02e235
Matt Hart <techtoysarefun@gmail.com>
2021-12-31 08:46:39 -0800

    Update imports for 'history'

--- application/history/routes.py
+++ application/history/routes.py
@@ -1,16 +1,6 @@
-import os
-import secrets
-from PIL import Image
-from flask import flash, redirect, render_template, request, session, url_for, Blueprint
-from flask_mail import Message
-from application import app, nav_avatar, mail
-from werkzeug.security import check_password_hash, generate_password_hash
-from werkzeug.utils import secure_filename
-import datetime
-import re
+from flask import flash, redirect, render_template, session, url_for, Blueprint
 from application.helpers import login_required
 from application import db
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 history = Blueprint('history', __name__)
 
