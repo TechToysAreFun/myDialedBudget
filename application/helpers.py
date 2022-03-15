8a55aeca9a493ea51d4151203dd3c5752cde934f
Matt Hart <techtoysarefun@gmail.com>
2022-03-14 19:22:18 -0700

    Add next=request.url to decorated_function

--- application/helpers.py
+++ application/helpers.py
@@ -1,16 +1,16 @@
 # The contents of this file are copied directly from Harvard's cs50x Finance source code
-from flask import redirect, session
+from flask import redirect, session, request
 from functools import wraps
 
 def login_required(f):
     """
     Decorate routes to require login.
-    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    https://flask.palletsprojects.com/en/2.0.x/patterns/viewdecorators/
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
-            return redirect("/login")
+            return redirect("/login", next=request.url)
         return f(*args, **kwargs)
     return decorated_function
 
